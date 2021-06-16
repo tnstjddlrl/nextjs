999a836e361c496757f8996aed865bdba22bdf5d
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-06-16 14:33:11 +0900

    헤드 수정

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>냥냥이!</title>
+        <link rel="icon">😺</link>
       </Head>
 
       <main className={styles.main}>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        Powered by Kwon sun seong
       </footer>
     </div>
   )
