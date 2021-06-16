2a6b5b97da9ab6e7eebbbd5ab05a172e50264fee
DESKTOP-V0TP74J\user <tnstjddlrl@naver.com>
2021-06-16 14:35:08 +0900

    내용 수정

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,13 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <h1 className={styles.title}>
+          안녕! 냥냥이!
+        </h1>
+
+        <p className={styles.description}>
+          여긴 냥냥이를 위한 페이지!
+        </p>
       </main>
 
       <footer className={styles.footer}>
